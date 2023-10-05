2b028c903f8586aa89440f0a4b09c5a6e700c487
Gad-Ongoro <gadongoro1@gmail.com>
2023-10-05 12:31:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(array, name) {
+    let drivers2 = array.filter((driver) => driver.toLowerCase() === name.toLowerCase() );
+
+    if(drivers2) {
+        return (drivers2);
+    } else{
+       return [];
+    };
+};
+
+
+function fuzzyMatch(array, nameInit) {
+    let drivers3 = array.filter((driver) => {
+        return (driver.startsWith(nameInit));
+    }    
+    )
+    return (drivers3);
+};
+
+
+function matchName(drivers, name) {
+    let drivers4 = drivers.filter(function(driverInfo) {
+        if (driverInfo.name === name){
+            return (driverInfo);
+        }
+    })
+
+    return (drivers4);
+};
